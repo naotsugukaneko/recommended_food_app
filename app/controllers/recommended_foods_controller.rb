8a9669ea710a3a0be742c01e0358636bdfa579d2
naotsugukaneko <nkk.407@gmail.com>
2021-06-21 16:47:43 +0900

    新規投稿機能の実装

--- app/controllers/recommended_foods_controller.rb
+++ app/controllers/recommended_foods_controller.rb
@@ -3,9 +3,14 @@ class RecommendedFoodsController < ApplicationController
     @foods = RecommendedFood.includes(:user).order(:created_at)
   end
 
-  def new; end
+  def new
+    @recommended_food = RecommendedFood.new
+  end
 
-  def create; end
+  def create
+    recommended_food = current_user.recommended_foods.create!(food_params)
+    redirect_to recommended_food
+  end
 
   def show; end
 
@@ -14,4 +19,10 @@ class RecommendedFoodsController < ApplicationController
   def update; end
 
   def destroy; end
+
+  private
+
+  def food_params
+    params.require(:recommended_food).permit(:name, :comment)
+  end
 end
